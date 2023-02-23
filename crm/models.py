f22ca5df3ec324d11d37849a8abb254894ae0d66
Sergey Grigorev <grigoscool@mail.ru>
2023-02-23 14:29:54 +0300

    add Order model

---
+++ crm/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_name = models.CharField(max_length=200, verbose_name='имя пользователя')
+    order_phone = models.CharField(max_length=200, verbose_name='телефон')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
