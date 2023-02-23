49221918aa9f0155a00ee550dc07ef2eb0231b97
Sergey Grigorev <grigoscool@mail.ru>
2023-02-23 16:44:50 +0300

    add static and forms

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Order
+from .forms import OrderForm
+
+
+def index(request):
+    orders = Order.objects.all()
+    form = OrderForm()
+    return render(request, 'crm/index.html', {'orders': orders,
+                                              'form': form})
+
+
+def thanks(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    item = Order.objects.create(order_name=name, order_phone=phone)
+
+    context = {
+        'name': name,
+        'phone': phone,
+        'item': item,
+    }
+    return render(request, 'crm/thanks.html', context)
