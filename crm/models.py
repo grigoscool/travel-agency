08fadbcfbf65da6442ff291c59c8f9ef41d506c6
Sergey Grigorev <grigoscool@mail.ru>
2023-02-24 17:36:22 +0300

    app CRM add models

--- crm/models.py
+++ crm/models.py
@@ -5,6 +5,9 @@ class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200, verbose_name='имя пользователя')
     order_phone = models.CharField(max_length=200, verbose_name='телефон')
+    order_status = models.ForeignKey(
+        'StatusCrm', on_delete=models.PROTECT,
+        verbose_name='Статус заказа', null=True, blank=True)
 
     def __str__(self):
         return self.order_name
@@ -12,3 +15,28 @@ class Order(models.Model):
     class Meta:
         verbose_name = 'заказ'
         verbose_name_plural = 'заказы'
+
+
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name='Статус')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class CommentCrm(models.Model):
+    comment_binding = models.ForeignKey(
+        Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(verbose_name='Текст комментария')
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
--- crm/views.py
+++ crm/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from .models import Order
 from cms.models import CmsSlider
 from price.models import PriceCard, PriceTable
+from .forms import OrderForm
+
 
 def index(request):
     slider_list = CmsSlider.objects.all()
     pc_1 = PriceCard.objects.get(pk=1)
     pc_2 = PriceCard.objects.get(pk=2)
     pc_3 = PriceCard.objects.get(pk=3)
+
+    form = OrderForm()
 
     services = PriceTable.objects.all()
     context = {
@@ -17,6 +21,7 @@ def index(request):
         'pc_2': pc_2,
         'pc_3': pc_3,
         'services': services,
+        'form': form,
     }
     return render(request, 'index.html', context)
 
