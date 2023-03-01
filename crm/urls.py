6354eed355a11d3ee8275e03ac6b9f801f51bf5a
Sergey Grigorev <grigoscool@mail.ru>
2023-03-01 10:43:12 +0300

    add templatetags

--- crm/urls.py
+++ crm/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('thanks', views.thanks, name='thanks'),
+    path('thanks/', views.thanks, name='thanks'),
 ]
